b6482dc81b6acf41df5e61cbb396ce6ec7688991
Hemapriya <165782576+Priyahhhh@users.noreply.github.com>
2026-09-13 17:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (38.39%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int low = 1;
+        int high = arr.length - 2;
+        if(arr.length == 1) {
+            return 0;}
+        if(arr[0] > arr[1]) {
+            return 0;
+        }
+        if(arr[arr.length - 1] > arr[arr.length - 2]) {
+            return arr.length - 1;
+        }
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+           return mid;
+            }
+            if(arr[mid] > arr[mid - 1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
